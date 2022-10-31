faa8b00ae0d54fe0ad436bca43b1d03fc8ce8d02
amassiet <amassiet@gmail.com>
2022-10-31 12:27:56 +0100

    focntion checkPropal

--- justeprix/justeprix.js
+++ justeprix/justeprix.js
@@ -8,6 +8,23 @@ document.getElementById("beginGame").addEventListener("click", function(){
     alert(NumberToFind);
 })
 
+document.getElementById("propal").addEventListener("click", function(){
+    checkPropal();
+})
+
 function getRandomInt(max){
     return Math.floor(Math.random()*max);
+}
+
+function checkPropal(){
+    if(numberToTest>NumberToFind){
+        console.log("Cest moins");
+    }
+    else if(numberToTest<NumberToFind){
+        console.log("Cest plus");
+    }
+    else if(numberToTest === NumberToFind){
+            console.log("Cest gagné")
+    
+    }
 }
